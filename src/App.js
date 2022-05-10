4aec5307c3fc1da6191a459acd44225ce33c37f7
Alrappie <alravimahesa@student.ub.ac.id>
2022-05-11 01:05:40 +0700

    pembaharuan

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Login,Daftar,Error } from './pages';
+import { Login,Daftar,Error,MenuUtama } from './pages';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css'
 
 
 function App() {
+
   return (
     <Router>
       <Routes>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/menu' element={<MenuUtama/>}/>
         <Route path='/daftar' element={<Daftar/>}/>
+        <Route path='/' exact element={<Login/>}/>
         <Route path='*' element={<Error/>}/>
       </Routes>
     </Router>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+
+
+function Navbar() {
+  return (
+    <nav className='nav'>
+        Mobil<span className='red'>Malang</span>
+        Profil
+        Riwayat
+        Keluar
+    </nav>
+  )
+}
+
+export default Navbar
